cd41e12427438114552f76be2d311a1768d755b8
acallvazq <korreoalba@gmail.com>
2022-05-15 01:10:12 +0200

    Cancelar reserva sin terminar

--- cancelar.sql
+++ cancelar.sql
@@ -2,16 +2,23 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS anularReserva //
 CREATE PROCEDURE anularReserva(
 IN idEntrada  int,
-IN idCliente int,
+IN idCliente int,  #id de cliente = correo
 IN idEspectaculo int, 
 OUT reembolso float 
 )
 BEGIN
-    DECLARE penalizacion int;
+    DECLARE precio, penalizacion int;
     DECLARE comienzo timestamp;
-    DECLARE usuario VARCHAR(10);
+    DECLARE estado, usuario VARCHAR(10);
+    DECLARE correoCliente int; #Autentificar el cliente
 
-    SELECT entradas.tipoUsuario INTO usuario FROM entradas WHERE entradas.horaReserva = idEntrada;
-    SELECT espectaculos.penalizacion INTO penalizacion
-    
-END
+    SELECT entradas.estado entradas.tipoUsuario, entradas.correoCliente INTO usuario, correoCliente FROM entradas WHERE entradas.horaReserva = idEntrada;
+    SELECT espectaculos.penalizacion INTO penalizacion FROM espectaculos WHERE espectaculos.nombreEsp = idEspectaculo;
+    SELECT tarifas.precio INTO precio FROM tarifas WHERE tarifas.tipoUsuario = usuario AND tarifas.nombreEsp = idEspectaculo;
+
+    IF idCliente = correoCliente then
+      IF (strcmp(estado, 'Reservado') == 0) then
+         SELECT * "Reserva encontrada"
+END //
+
+DELIMITER;
