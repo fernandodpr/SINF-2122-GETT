289f313b0088d25c1515ad4444b31594a706a443
martina.paramos <martinaparamos@gmail.com>
2022-05-15 01:05:32 +0200

    creo q completado. revisar.

--- newEvent.sql
+++ newEvent.sql
@@ -2,34 +2,41 @@
 USE proyecto;
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS novoEvento;
+DROP PROCEDURE IF EXISTS nuevoEvento;
 
-CREATE PROCEDURE novoEvento(
+CREATE PROCEDURE nuevoEvento(
 	IN espectaculo VARCHAR(20),
 	IN TipoEspec VARCHAR(20),
-	IN FechaProduc VARCHAR(20),
+	IN FechaProduc DATE,
 	IN productorain VARCHAR(20),
-	IN DateTime VARCHAR(20),
+	IN horario VARCHAR(20),
 	IN Recinto VARCHAR(50)
 )
 BEGIN
 	
-	IF ((SELECT COUNT(*) FROM espectaculos WHERE nombreEsp=espectaculo AND tipoEsp=TipoEspec AND fechaProduccion=FechaProduc AND productora=productorain ) < 1) THEN
-        	SELECT 'No se ha encotrado este espectaculo, creando uno nuevo. INCOMPLETO';
-		# CALL crearEspectaculo(espectaculo,TipoEspec,FechaProduc,productorain);	
-    	END IF;
-	IF ((SELECT COUNT(*) FROM horarios WHERE fechaYHora=DateTime) < 1) THEN
-        	SELECT 'No se ha encotrado un horario con estos datos. Creado. INCOMPLETO';
-		INSERT INTO horarios VALUES (DateTime);
-		
-    	END IF;
-	IF ((SELECT COUNT(*) FROM recintos WHERE direccion=Recinto) < 1) THEN
-        	SELECT 'No se ha encotrado un recinto. Añadalo usando el metodo addVenue. INCOMPLETO';
-    	END IF;
-
+	IF ((SELECT COUNT(*) FROM horarios WHERE fechaYHora=horario) < 1) THEN
+        SELECT 'No se ha encotrado un horario con estos datos. Creado uno nuevo.';
+		INSERT INTO horarios VALUES (horario);	
+    END IF;
 	
+	IF ((SELECT COUNT(*) FROM recintos WHERE direccion=Recinto) < 1) THEN
+        SELECT 'No se ha encotrado un recinto.';
+		INSERT INTO recintos (direccion) VALUES (Recinto);	
+	END IF;
+
+	IF ((SELECT COUNT(*) FROM horariosRecintos WHERE direccion=Recinto AND fechaYHora=horario) < 1) THEN
+        SELECT 'No se ha encotrado la tupla horario-recinto';
+		INSERT INTO horariosRecintos VALUES (horario, Recinto);	
+	END IF;
 
 
+	IF ((SELECT COUNT(*) FROM espectaculos WHERE nombreEsp=espectaculo AND tipoEsp=TipoEspec AND fechaProduccion=FechaProduc AND productora=productorain) < 1) THEN
+        SELECT 'No se ha encotrado este espectaculo, creando uno nuevo.';
+		CALL crearEspectaculo(espectaculo,TipoEspec,FechaProduc,productorain, NULL, 0,0,0,0);	
+    END IF;
+
+	INSERT IGNORE INTO eventos VALUES (espectaculo, TipoEspec, FechaProduc, productorain, horario, Recinto);
+	
 END //
 
 DELIMITER ;
