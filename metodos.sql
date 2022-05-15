886355556424ab5e642a548649bd2fbc9e3506b1
martina.paramos <martinaparamos@gmail.com>
2022-05-15 18:25:32 +0200

    metodo crearEspectaculo metido aqui

--- metodos.sql
+++ metodos.sql
@@ -20,6 +20,7 @@ END
 DROP PROCEDURE IF EXISTS listarEventosDisponibles//
 CREATE PROCEDURE listarEventosDisponibles()
 BEGIN 
+    DROP VIEW IF EXISTS eventosView;
     CREATE VIEW eventosView AS SELECT tipoEsp, nombreEsp, direccion, fechaYHora 
     FROM eventos
     WHERE fechaYHora > now()
@@ -27,3 +28,12 @@ BEGIN
 
     SELECT * FROM eventosView;
 END
+//
+
+
+DROP PROCEDURE IF EXISTS crearEspectaculo//
+CREATE PROCEDURE crearEspectaculo(IN nombreEsp VARCHAR(20), IN tipoEsp VARCHAR(20), IN fechaProd DATE, IN productora VARCHAR(20), IN participantes VARCHAR(30), IN penalizacion INT, IN tValidezReserva INT, IN tAntelacionReserva INT, IN tCancelacion INT)
+BEGIN 
+	INSERT INTO espectaculos VALUES (nombreEsp, tipoEsp, fechaProd, productora, participantes, penalizacion, tValidezReserva, tAntelacionReserva, tCancelacion);
+END
+//
