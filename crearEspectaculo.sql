268835d8206b4d8b6b3107704e18cd6d50ebfde8
Fernandodpr <contacto@fernandodpr.es>
2022-05-16 20:13:57 +0200

    La hora

--- crearEspectaculo.sql
+++ crearEspectaculo.sql
@@ -1,7 +1,7 @@
 
 DELIMITER //
 DROP PROCEDURE IF EXISTS crearEspectaculo//
-CREATE PROCEDURE crearEspectaculo(IN nombreEsp VARCHAR(20), IN tipoEsp VARCHAR(20), IN fechaProd DATE, IN productora VARCHAR(20), IN participantes VARCHAR(30), IN penalizacion INT, IN tValidezReserva INT, IN tAntelacionReserva INT, IN tCancelacion INT)
+CREATE PROCEDURE crearEspectaculo(IN nombreEsp VARCHAR(20), IN tipoEsp VARCHAR(20), IN fechaProd DATE, IN productora VARCHAR(20), IN participantes VARCHAR(30), IN penalizacion INT, IN tValidezReserva TIME, IN tAntelacionReserva TIME, IN tCancelacion TIME)
 BEGIN 
 	INSERT INTO espectaculos VALUES (nombreEsp, tipoEsp, fechaProd, productora, participantes, penalizacion, tValidezReserva, tAntelacionReserva, tCancelacion);
 END
