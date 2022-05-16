4adaaae658dc0de19d28f19b7a75cdfd88d9e8e7
Fernandodpr <contacto@fernandodpr.es>
2022-05-16 20:14:09 +0200

    Evento que funciona

--- eventoScrubEstadoEvento.sql
+++ eventoScrubEstadoEvento.sql
@@ -2,28 +2,18 @@ SET GLOBAL event_scheduler = ON;
 #SET GLOBAL event_scheduler = OFF;
 USE proyecto;
 DROP EVENT IF EXISTS ScrubEventos;
-
+DELIMITER //
 CREATE EVENT ScrubEventos
-ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 MINUTE
-ON COMPLETION PRESERVE
+  ON SCHEDULE EVERY 1 MINUTE STARTS NOW()
+  ON COMPLETION PRESERVE
 DO
-    UPDATE eventos SET estado='Finalizado' WHERE fechaYHora=Now();
-    #NO SE PODRA REALIZAR UNA RESERVA(CERRADO) SI FALTAN MENOS DE T2 MINUTOS PARA EL COMIENZO
-    SET @t2 := (SELECT tAntelacionReserva FROM espectaculos WHERE ) 
-    SELECT espectaculos.nombreEsp,eventos.tipoEsp ,tValidezReserva, tAntelacionReserva, tCancelacion  
-                FROM eventos INNER JOIN espectaculos ON eventos.nombreEsp=espectaculos.nombreEsp 
-                AND eventos.tipoEsp=espectaculos.tipoEsp AND eventos.productora=espectaculos.productora 
-                AND eventos.fechaProduccion=espectaculos.fechaProduccion;
-
-    UPDATE eventos,espectaculos SET eventos.estado='Cerrado' WHERE SUBTIME( LastModifiedDate, MINUTOS(pero tienen que estar en TIME)) )=Now();
+BEGIN
     
-  
-    SELECT espectaculos.nombreEsp,eventos.tipoEsp ,tValidezReserva, tAntelacionReserva, tCancelacion  FROM eventos INNER JOIN espectaculos ON eventos.nombreEsp=espectaculos.nombreEsp AND eventos.tipoEsp=espectaculos.tipoEsp AND eventos.productora=espectaculos.productora AND eventos.fechaProduccion=espectaculos.fechaProduccion;
-    
-    UPDATE eventos SET Estado='Cerrado' WHERE 
-                                tAntelacionReserva < MINUTE((SELECT fechaYHora FROM espectaculos, eventos WHERE espectaculo.nombreEsp = eventos.nombreEsp AND espectaculo.tipoEsp = eventos.nombreEsp  
-                                AND espectaculo.fechaProduccion = eventos.fechaProduccion AND espectaculo.productora = eventos.productora) - now());
-
     UPDATE eventos INNER JOIN espectaculos ON eventos.nombreEsp=espectaculos.nombreEsp AND eventos.tipoEsp=espectaculos.tipoEsp AND eventos.productora=espectaculos.productora AND eventos.fechaProduccion=espectaculos.fechaProduccion
     SET eventos.estado='Cerrado'  
-    WHERE SUBTIME(eventos.fechaYHora,espectaculos.tAntelacionReserva)<NOW();  
+    WHERE SUBTIME(eventos.fechaYHora,espectaculos.tAntelacionReserva)<NOW();
+\.
+    UPDATE eventos INNER JOIN espectaculos ON eventos.nombreEsp=espectaculos.nombreEsp AND eventos.tipoEsp=espectaculos.tipoEsp AND eventos.productora=espectaculos.productora AND eventos.fechaProduccion=espectaculos.fechaProduccion
+    SET eventos.estado='Finalizado'  
+    WHERE eventos.fechaYHora<NOW();
+END //
