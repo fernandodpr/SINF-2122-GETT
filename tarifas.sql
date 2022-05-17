e0c54948c515f8297b69345eb75bb0e76709ab8d
omardl <105445540+omardl@users.noreply.github.com>
2022-05-17 12:14:19 -0400

    Update tarifas.sql

--- tarifas.sql
+++ tarifas.sql
@@ -2,8 +2,11 @@ drop procedure if exists crearTarifa;
 
 delimiter //
 
-create procedure crearTarifa(in nomEsp varchar(20), in produ varchar(20), in tipEsp varchar(20), in fechaP date, in fecha date, in direc varchar(50), in nomGrada varchar(20), in tipoUs enum('bebe', 'infantil', 'juvenil', 'adulto', 'jubilado'), in precio numeric, in maxReservas int) begin
+create procedure crearTarifa(in nomEsp varchar(20), in produ varchar(20), in tipEsp varchar(20), in fechaP date, in fecha date, in direc varchar(50), in nomGrada varchar(20), in tipoUs enum('bebe', 'infantil', 'juvenil', 'adulto', 'jubilado'), in precio numeric, in maxReservas int) begin 
+        DECLARE inicio TIMESTAMP(6);
+        SET inicio = CURRENT_TIMESTAMP(6);
 	insert into tarifas values(tipoUs, precio, maxReservas, nomGrada, nomEsp, tipEsp, fechaP, produ, fecha, direc);
+	SELECT timestampdiff(MICROSECOND, inicio, CURRENT_TIMESTAMP(6))/1000000 AS 'Tiempo de ejecucion';
 end//
 
 delimiter ;
