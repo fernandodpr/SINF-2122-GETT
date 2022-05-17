c05f41a479fe27db364f70abde2ad9027755e8e1
martina.paramos <martinaparamos@gmail.com>
2022-05-17 17:16:52 +0200

    .

--- metodos.sql
+++ metodos.sql
@@ -43,4 +43,41 @@ END
 //
 
 
+DROP PROCEDURE IF EXISTS modificarEspectaculo//
+CREATE PROCEDURE modificarEspectaculo(
+    IN INnombreEsp VARCHAR(30),
+    IN INtipoEsp varchar(30),
+    IN INfechaProduccion DATE,
+    IN INproductora VARCHAR(30) ,
+    IN INpenalizacion INT,
+    IN t1 TIME,
+    IN t2 TIME,
+    IN t3 TIME
+)
+BEGIN 
+    #SET @espectaculo = (SELECT * FROM espectaculos WHERE nombreEsp=INnombreEsp AND tipoEsp=INtipoEsp AND fechaProduccion=INfechaProduccion AND productora=INproductora);
+
+    IF (penalizacion IS NOT NULL) THEN
+        UPDATE espectaculos SET penalizacion = INpenalizacion
+        WHERE nombreEsp=nombreEsp AND tipoEsp=tipoEsp AND fechaProduccion=fechaProduccion AND productora=productora;
+    END IF;
+
+    IF (t1 IS NOT NULL) THEN
+        UPDATE espectaculos SET tValidezReserva = t1 
+        WHERE nombreEsp=nombreEsp AND tipoEsp=tipoEsp AND fechaProduccion=fechaProduccion AND productora=productora;
+    END IF;
+
+    IF (t2 IS NOT NULL) THEN
+        UPDATE espectaculos SET tAntelacionReserva = t2
+        WHERE nombreEsp=nombreEsp AND tipoEsp=tipoEsp AND fechaProduccion=fechaProduccion AND productora=productora;
+    END IF;
+
+    IF (t3 IS NOT NULL) THEN
+        UPDATE espectaculos SET tCancelacion = t3
+        WHERE nombreEsp=nombreEsp AND tipoEsp=tipoEsp AND fechaProduccion=fechaProduccion AND productora=productora;
+    END IF;
+END
+//
+
+
 
