1ad93a3bb3cba0279dd8146545589419b7f9907d
omardl <105445540+omardl@users.noreply.github.com>
2022-05-17 12:16:08 -0400

    Update trigDeleteEntrada.sql

--- trigDeleteEntrada.sql
+++ trigDeleteEntrada.sql
@@ -4,9 +4,11 @@ CREATE TRIGGER trigDeleteEntrada
 AFTER DELETE
 ON entradas FOR EACH ROW
 BEGIN
+    DECLARE inicio TIMESTAMP(6);
+    SET inicio = CURRENT_TIMESTAMP(6);
     INSERT INTO cancelaciones VALUES(old.formaPago,old.horaReserva,old.correoCliente,old.tipoUsuario,old.asientoLocalidad,old.nombreGrada,old.nombreEsp,old.tipoEsp,old.fechaProduccion,old.productora,old.fechaYHora,old.direccion,NOW());
     UPDATE localidades SET estado = "Libre" WHERE asientoLocalidad=old.asientoLocalidad AND nombreGrada = old.nombreGrada 
         AND nombreEsp=old.nombreEsp AND tipoEsp=old.tipoEsp AND fechaProduccion=old.fechaProduccion 
         AND productora=old.productora AND fechaYHora=old.fechaYHora AND direccion=old.direccion;
-
+    SELECT timestampdiff(MICROSECOND, inicio, CURRENT_TIMESTAMP(6))/1000000 AS 'Tiempo de ejecucion';
 END//
